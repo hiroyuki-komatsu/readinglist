1dfe09dff755e7333c750ee057209c1e38f2627e
Hiroyuki Komatsu <hiroyuki.komatsu@gmail.com>
2024-03-17 18:19:45 +0900

    Refactoring

--- crx/background.js
+++ crx/background.js
@@ -1,6 +1,21 @@
-function setIcon(isAdded) {
-  const icon = isAdded ? "icon_added.png" : "icon.png";
-  chrome.action.setIcon({path: {"128": icon}});
+const toggleEntryId = "toggle_entry";
+const weeklySummaryId = "weekly_summary";
+
+function getIconPath(isAdded) {
+  return isAdded ? "icon_added.png" : "icon.png";
+}
+
+function getToggleEntryTitle(isAdded) {
+  return isAdded ? "Remove from Reading List" : "Add to Reading List";
+}
+
+function setStatus(isAdded) {
+  chrome.action.setIcon({path: {"128": getIconPath(isAdded)}});
+
+  chrome.contextMenus.update(toggleEntryId, {
+    "title": getToggleEntryTitle(isAdded),
+    "contexts": ["page"],  // Right click on the page
+  });
 }
 
 function addEntry(tab) {
@@ -15,10 +30,10 @@ function toggleEntry(tab) {
   checkEntry(tab, (isAdded) => {
     if (isAdded) {
       chrome.readingList.removeEntry({url: tab.url});
-      setIcon(false);
+      setStatus(false);
     } else {
       addEntry(tab);
-      setIcon(true);
+      setStatus(true);
     }
   });
 }
@@ -35,20 +50,18 @@ async function checkEntry(tab, callback) {
   });
 }
 
-async function updateIcon(tab) {
-  console.log("tab.url: " + tab.url);
-  console.log("tab.title: " + tab.title);
-  checkEntry(tab, setIcon);
+async function updateStatus(tab) {
+  checkEntry(tab, setStatus);
 }
 
 async function onTabActivated(info) {
-  chrome.tabs.get(info.tabId, updateIcon);
+  chrome.tabs.get(info.tabId, updateStatus);
 }
 
 async function onWindowFocusChanged(windowId) {
   chrome.tabs.query({"active": true, "windowId": windowId}, (tabs) => {
     if (tabs.length > 0) {
-      updateIcon(tabs[0]);
+      updateStatus(tabs[0]);
     }
   });
 }
@@ -57,29 +70,33 @@ function openSummary() {
   chrome.tabs.create({"url": "readinglist.html"});
 }
 
+// Listeners
+
 chrome.action.onClicked.addListener(toggleEntry);
 
 chrome.tabs.onActivated.addListener(onTabActivated);
 chrome.windows.onFocusChanged.addListener(onWindowFocusChanged);
 
+// Context menu
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
-    "id": "add_entry",
-    "title": "Add to Reading List",
+    "id": toggleEntryId,
+    "title": getToggleEntryTitle(/*isAdded=*/false),
     "contexts": ["page"],  // Right click on the page
   });
   chrome.contextMenus.create({
-    "id": "weekly_summary",
+    "id": weeklySummaryId,
     "title": "Weekly summary",
     "contexts": ["action"],  // Right click on the extension icon
   });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "add_entry") {
-    addEntry(tab);
+  if (info.menuItemId === toggleEntryId) {
+    toggleEntry(tab);
   }
-  if (info.menuItemId === "weekly_summary") {
+  if (info.menuItemId === weeklySummaryId) {
     openSummary();
   }
 });
