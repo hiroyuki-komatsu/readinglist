a029c5ad6b945755ef9677333f98f6b7a23ed407
Hiroyuki Komatsu <hiroyuki.komatsu@gmail.com>
2024-03-17 14:06:44 +0900

    create addEntry

--- crx/background.js
+++ crx/background.js
@@ -1,19 +1,35 @@
-function openPage(tab) {
+function addEntry(tab) {
+  chrome.readingList.addEntry({
+    title: tab.title,
+    url: tab.url,
+    hasBeenRead: false,
+  });
+}
+
+function openSummary() {
   chrome.tabs.create({"url": "readinglist.html"});
 }
 
-chrome.action.onClicked.addListener(openPage);
+chrome.action.onClicked.addListener(addEntry);
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
+    "id": "add_entry",
+    "title": "Add to Reading List",
+    "contexts": ["page"],  // Right click on the page
+  });
+  chrome.contextMenus.create({
     "id": "weekly_summary",
     "title": "Weekly summary",
-    "contexts": ["action"],
+    "contexts": ["action"],  // Right click on the extension icon
   });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "add_entry") {
+    addEntry(tab);
+  }
   if (info.menuItemId === "weekly_summary") {
-    chrome.tabs.create({"url": "readinglist.html"});
+    openSummary();
   }
 });
