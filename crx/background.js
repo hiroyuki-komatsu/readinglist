85614b8df042283506d6e166525b522f4fe3d09b
Hiroyuki Komatsu <hiroyuki.komatsu@gmail.com>
2024-03-17 13:49:33 +0900

    Add a menu for "Weekly summary"

--- crx/background.js
+++ crx/background.js
@@ -3,3 +3,17 @@ function openPage(tab) {
 }
 
 chrome.action.onClicked.addListener(openPage);
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    "id": "weekly_summary",
+    "title": "Weekly summary",
+    "contexts": ["action"],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "weekly_summary") {
+    chrome.tabs.create({"url": "readinglist.html"});
+  }
+});
